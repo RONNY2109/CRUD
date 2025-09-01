939f23d7f5a79e3b6fa363daebe9d013abeda82b
Alejandro Ramirez <alejandrocohete@gmail.com>
2025-09-01 14:07:10 -0400

    Fix Put conflit

--- Routers/lenguajes.js
+++ Routers/lenguajes.js
@@ -44,7 +44,7 @@ aleRouter.get('/:titulo/:nivel', (req, res) => {
 aleRouter.post('/', (req, res) => {
     let cursoNuevo = req.body;
     lenguajes.push(cursoNuevo);
-    res.send(cursoNuevo);
+    res.send(lenguajes);
 })
 
 //Metodo PUT para actualizar un curso existente
